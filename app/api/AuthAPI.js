0b26ca2e27a41bc87083b902d5273663b152bcf5
shaya <shayasms128@gmail.com>
2017-05-11 09:13:47 +0300

    make a fake sign up request using a timeout

--- app/api/AuthAPI.js
+++ app/api/AuthAPI.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   signUp:function (email, pass) {
-     return Promise.resolve('aviv');
+    return new Promise(function (resolve, reject) {
+          setTimeout(function () {
+              resolve("sign in success");
+          }, 1000);
+      });
   }
 }
