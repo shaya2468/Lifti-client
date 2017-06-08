1de42f2260bf04f322458a53cca5e9c27b99e43a
shaya <shayasms128@gmail.com>
2017-06-08 11:23:13 +0300

    add navigation bar

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -8,14 +8,37 @@ import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
 import * as actions from 'actions';
 import {
-  Redirect
+  Redirect,  Route,
+  Link
 } from 'react-router-dom'
 
 
 export class Home extends React.Component{
 
+  onLogout = (e) => {
+    e.preventDefault();
+    var {dispatch} = this.props;
+    dispatch(actions.startLogout());
+  }
+
   render(){
     var {uid} = this.props;
+
+    const routes = [
+  { path: '/home',
+    exact: true,
+    main: () => <Dummy/>
+
+  },
+  { path: '/home/group',
+    main: () => <h2>group!</h2>
+  },
+  { path: '/home/lift',
+    main: () => <h2>lift!</h2>
+  }
+]
+
+
     if (typeof uid==='undefined'){
       return (
         <Redirect to={{
@@ -25,8 +48,27 @@ export class Home extends React.Component{
     }else{
       return (
         <div id="home_full">
-          <Nav/>
-          <Dummy/>
+          <div id="home_nav">
+            <Link to="/home" className="btn_nav">Home</Link>
+            <Link to="/home/group" className="btn_nav">Group</Link>
+            <Link to="/home/lift" className="btn_nav">Lift</Link>
+            <button type="button" name="button" className="btn_nav" onClick={ this.onLogout }>Logout</button>
+          </div>
+          {routes.map((route, index) => (
+            // You can render a <Route> in as many places
+            // as you want in your app. It will render along
+            // with any other <Route>s that also match the URL.
+            // So, a sidebar or breadcrumbs or anything else
+            // that requires you to render multiple things
+            // in multiple places at the same URL is nothing
+            // more than multiple <Route>s.
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))}
         </div>
       )
     }
