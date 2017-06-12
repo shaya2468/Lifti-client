52b7705deb0fc179e4175282a9303404d12e4976
shaya <shayasms128@gmail.com>
2017-06-12 17:17:35 +0300

    add an image to the group page, right now it’s just dummy

--- app/components/GroupInfo.jsx
+++ app/components/GroupInfo.jsx
@@ -15,7 +15,10 @@ export class GroupInfo extends React.Component{
 
     return (
       <div id="dummy">
-        <h1>Welcome to the group page {groupId}!!</h1>
+        <h1 id="group-page-header">Welcome to the group page {groupId}!!</h1>
+          <img id="group-page-image"
+           src="https://scontent.ftlv1-1.fna.fbcdn.net/v/t1.0-9/12112368_10204247364098648_1699726661887914998_n.jpg?oh=c1fc1a641b84073805c4b8b4483abbc3&oe=59DE23EA"
+           alt="loading..." />
       </div>
     )
   }
