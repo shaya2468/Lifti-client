d7970529eb3ad84959d0eae273f6aae89f1f1a49
shaya <shayasms128@gmail.com>
2017-06-20 15:37:57 +0300

    add some important moment things to play.js

--- app/playground/play.js
+++ app/playground/play.js
@@ -1,10 +1,35 @@
-console.log('stupid');
-// function multiply(x, y) {
-//     return x - y;
-// }
+
+
+
+
+
+//moment stuff don't touch
+//https://stackoverflow.com/questions/26392280/using-momentjs-to-convert-date-to-epoch-then-back-to-date
+// var moment = require('moment');
+// console.log('welcome');
+// var timestamp = moment("10/15/2014 19:00", "MM/DD/YYYY HH:mm").unix();
+// console.log(timestamp);
 //
-// let multiplyBy10 = multiply.bind(null, 10);
-// console.log(multiplyBy10(15));
+// var message = moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+// console.log(message);
+//
+// var hour = moment.unix(timestamp).format('H');
+// console.log(hour);
+//
+// var hour2 = moment.unix(timestamp).format('h a');
+// console.log(hour2);
 
 
-var json = {}
+var moment = require('moment');
+console.log('welcome');
+var timestamp = moment("2017-06-29 14:33", "YYYY/MM/DD HH:mm").unix();
+console.log(timestamp);
+
+var message = moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+console.log(message);
+
+var hour = moment.unix(timestamp).format('H');
+console.log(hour);
+
+var hour2 = moment.unix(timestamp).format('h a');
+console.log(hour2);
