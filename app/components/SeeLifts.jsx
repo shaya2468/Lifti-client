52629cb084f5cc4d9a3917d5c576c4e34f8d2252
shaya <shayasms128@gmail.com>
2017-06-26 17:29:10 +0300

    add entire lift screen with dummy data

--- app/components/SeeLifts.jsx
+++ app/components/SeeLifts.jsx
@@ -7,7 +7,7 @@ export class SeeLifts extends React.Component{
   render() {
 
     return (
-      <div id="dummy">
+      <div id="lifts-list">
 
           <form id="see-lift-form" >
             <ul>
@@ -66,7 +66,232 @@ export class SeeLifts extends React.Component{
           </form>
 
         <SearchForm/>
-        <CarSVG/>
+        <div >
+
+        <section className="responsive2">
+
+
+
+          <ul >
+             <li className="list-item-lift">
+                  <div className="lift-address-wrapper">
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Leaving from</h3>
+                      <h4 className="lift-street">Rachel Alter 34, lod</h4>
+                    </div>
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Destination</h3>
+                      <h4 className="lift-street">Alenbi 22, Tel aviv</h4>
+                    </div>
+
+                  </div>
+
+                    <h3 className="when-leave">Departing on August 21, 2017 at 11:30am</h3>
+
+                    <div className="driver-and-join">
+                      <div className="driver-of-ride">
+                        <h4>Driver is Walter white</h4>
+                        <div className="ride-image-strip">
+                          <img src="https://goo.gl/jiiEQx" alt="" text="aaa"/>
+                        </div>
+                      </div>
+                      <div className="join-ride-layout">
+                        <CarSVG/>
+                        <h4 className="join-ride-text">join ride</h4>
+                      </div>
+                    </div>
+
+                  <h4>who's confirmed?</h4>
+                  <div className="ride-image-strip confirmed-list">
+                      <img src="https://goo.gl/e3fq8V" alt="" text="aaa"/>
+                      <img src="https://goo.gl/u5bPP7" alt=""/>
+                      <img src="https://goo.gl/giKRBV" alt=""/>
+                      <img src="https://goo.gl/LxAz1b" alt="" text="aaa"/>
+
+                  </div>
+
+                  <p className="ride-comments">The airconditioning is going to be on 12 degrees, so if you can't handle it please don't come. In addition
+                      we will be listening to norwegian death metal in full volume the entire way. Also, please be on time!! </p>
+              </li>
+             <li className="list-item-lift">
+                  <div className="lift-address-wrapper">
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Leaving from</h3>
+                      <h4 className="lift-street">Rachel Alter 34, lod</h4>
+                    </div>
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Destination</h3>
+                      <h4 className="lift-street">Alenbi 22, Tel aviv</h4>
+                    </div>
+
+                  </div>
+
+                    <h3 className="when-leave">Departing on August 21, 2017 at 11:30am</h3>
+
+                    <div className="driver-and-join">
+                      <div className="driver-of-ride">
+                        <h4>Driver is Walter white</h4>
+                        <div className="ride-image-strip">
+                          <img src="https://goo.gl/jiiEQx" alt="" text="aaa"/>
+                        </div>
+                      </div>
+                      <div className="join-ride-layout">
+                        <CarSVG/>
+                        <h4 className="join-ride-text">join ride</h4>
+                      </div>
+                    </div>
+
+                  <h4>who's confirmed?</h4>
+                  <div className="ride-image-strip confirmed-list">
+                      <img src="https://goo.gl/e3fq8V" alt="" text="aaa"/>
+                      <img src="https://goo.gl/u5bPP7" alt=""/>
+                      <img src="https://goo.gl/giKRBV" alt=""/>
+                      <img src="https://goo.gl/LxAz1b" alt="" text="aaa"/>
+
+                  </div>
+
+                  <p className="ride-comments">The airconditioning is going to be on 12 degrees, so if you can't handle it please don't come. In addition
+                      we will be listening to norwegian death metal in full volume the entire way. Also, please be on time!! </p>
+              </li>
+             <li className="list-item-lift">
+                  <div className="lift-address-wrapper">
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Leaving from</h3>
+                      <h4 className="lift-street">Rachel Alter 34, lod</h4>
+                    </div>
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Destination</h3>
+                      <h4 className="lift-street">Alenbi 22, Tel aviv</h4>
+                    </div>
+
+                  </div>
+
+                    <h3 className="when-leave">Departing on August 21, 2017 at 11:30am</h3>
+
+                    <div className="driver-and-join">
+                      <div className="driver-of-ride">
+                        <h4>Driver is Walter white</h4>
+                        <div className="ride-image-strip">
+                          <img src="https://goo.gl/jiiEQx" alt="" text="aaa"/>
+                        </div>
+                      </div>
+                      <div className="join-ride-layout">
+                        <CarSVG/>
+                        <h4 className="join-ride-text">join ride</h4>
+                      </div>
+                    </div>
+
+                  <h4>who's confirmed?</h4>
+                  <div className="ride-image-strip confirmed-list">
+                      <img src="https://goo.gl/e3fq8V" alt="" text="aaa"/>
+                      <img src="https://goo.gl/u5bPP7" alt=""/>
+                      <img src="https://goo.gl/giKRBV" alt=""/>
+                      <img src="https://goo.gl/LxAz1b" alt="" text="aaa"/>
+
+                  </div>
+
+                  <p className="ride-comments">The airconditioning is going to be on 12 degrees, so if you can't handle it please don't come. In addition
+                      we will be listening to norwegian death metal in full volume the entire way. Also, please be on time!! </p>
+              </li>
+             <li className="list-item-lift">
+                  <div className="lift-address-wrapper">
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Leaving from</h3>
+                      <h4 className="lift-street">Rachel Alter 34, lod</h4>
+                    </div>
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Destination</h3>
+                      <h4 className="lift-street">Alenbi 22, Tel aviv</h4>
+                    </div>
+
+                  </div>
+
+                    <h3 className="when-leave">Departing on August 21, 2017 at 11:30am</h3>
+
+                    <div className="driver-and-join">
+                      <div className="driver-of-ride">
+                        <h4>Driver is Walter white</h4>
+                        <div className="ride-image-strip">
+                          <img src="https://goo.gl/jiiEQx" alt="" text="aaa"/>
+                        </div>
+                      </div>
+                      <div className="join-ride-layout">
+                        <CarSVG/>
+                        <h4 className="join-ride-text">join ride</h4>
+                      </div>
+                    </div>
+
+                  <h4>who's confirmed?</h4>
+                  <div className="ride-image-strip confirmed-list">
+                      <img src="https://goo.gl/e3fq8V" alt="" text="aaa"/>
+                      <img src="https://goo.gl/u5bPP7" alt=""/>
+                      <img src="https://goo.gl/giKRBV" alt=""/>
+                      <img src="https://goo.gl/LxAz1b" alt="" text="aaa"/>
+
+                  </div>
+
+                  <p className="ride-comments">The airconditioning is going to be on 12 degrees, so if you can't handle it please don't come. In addition
+                      we will be listening to norwegian death metal in full volume the entire way. Also, please be on time!! </p>
+              </li>
+             <li className="list-item-lift">
+                  <div className="lift-address-wrapper">
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Leaving from</h3>
+                      <h4 className="lift-street">Rachel Alter 34, lod</h4>
+                    </div>
+
+                    <div className="lift-address">
+                      <h3 className="single-lift-top-title">Destination</h3>
+                      <h4 className="lift-street">Alenbi 22, Tel aviv</h4>
+                    </div>
+
+                  </div>
+
+                    <h3 className="when-leave">Departing on August 21, 2017 at 11:30am</h3>
+
+                    <div className="driver-and-join">
+                      <div className="driver-of-ride">
+                        <h4>Driver is Walter white</h4>
+                        <div className="ride-image-strip">
+                          <img src="https://goo.gl/jiiEQx" alt="" text="aaa"/>
+                        </div>
+                      </div>
+                      <div className="join-ride-layout">
+                        <CarSVG/>
+                        <h4 className="join-ride-text">join ride</h4>
+                      </div>
+                    </div>
+
+                  <h4>who's confirmed?</h4>
+                  <div className="ride-image-strip confirmed-list">
+                      <img src="https://goo.gl/e3fq8V" alt="" text="aaa"/>
+                      <img src="https://goo.gl/u5bPP7" alt=""/>
+                      <img src="https://goo.gl/giKRBV" alt=""/>
+                      <img src="https://goo.gl/LxAz1b" alt="" text="aaa"/>
+
+                  </div>
+
+                  <p className="ride-comments">The airconditioning is going to be on 12 degrees, so if you can't handle it please don't come. In addition
+                      we will be listening to norwegian death metal in full volume the entire way. Also, please be on time!! </p>
+              </li>
+
+
+
+
+
+          </ul>
+              </section>
+
+        </div>
       </div>
     )
   }
