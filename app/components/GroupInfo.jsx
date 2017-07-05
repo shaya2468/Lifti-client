c9fe58b46441f2c487de90c77b262d0935ad35e7
shaya <shayasms128@gmail.com>
2017-07-05 15:47:00 +0300

    add title of members and change some styles

--- app/components/GroupInfo.jsx
+++ app/components/GroupInfo.jsx
@@ -92,23 +92,26 @@ export class GroupInfo extends React.Component{
                 <li className="group-main-info-layout group-join-layout">
 
                     <h5 id="group-interested">Looks interesting?</h5>
-                    <label htmlFor="comments" id="label-join-group">Add message:</label>
+                    <label htmlFor="comments" id="label-join-group">Add join message:</label>
                    <textarea ref= "message" cols="46" rows="3" name="comments" id="text-join-group" ></textarea>
 
                   <button id="send-permission-request-button" onClick={ (e) => {
                       var message = this.refs.message.value;
                       this.sendJoinRequest(message)
-                    }}>join group</button>
+                    }}>send request</button>
 
                 </li>
               }
 
           </ul>
 
+            <div id="group-members-title-layout" className="group-main-info-layout group-members-title">
+              <h2 >
+                  members
+              </h2>
+              <MembersSVG/>
+            </div>
 
-            <h2 className="group-main-info-layout group-members-title">
-                members
-            </h2>
 
           <div id="who-in-group">
             <ul className="group-users-layout ">
@@ -133,51 +136,19 @@ export class GroupInfo extends React.Component{
   }
 }
 
-// class UserStatus extends React.Component{
-//
-//   constructor(props) {
-//     super(props);
-//   }
-//
-//   render() {
-//     var {status} = this.props;
-//
-//     if (status === 'non_member'){
-//       return(
-//         <div id="join-group-layout">
-//
-//             <label htmlFor="comments" id="label-join-group" </label></label></label>oin message</label>
-//             <textarea ref= "message" cols="46" rows="3" name="comments" id="text-join-group" ></textarea>
-//
-//           <button id="send-permission-request-button" onClick={ (e) => {
-//               var message = this.refs.message.value;
-//               this.props.sendJoinRequest(message)
-//             }}>join group</button>
-//         </div>
-//
-//       )
-//     }
-//     var message;
-//
-//     if (status === 'permission_request_sent'){
-//       message = 'join request sent'
-//     }
-//     if (status === 'manager'){
-//       message = 'you are the manager'
-//     }
-//     if (status === 'member'){
-//       message = 'you are a member'
-//     }
-//
-//
-//     return (
-//       <h1 id="group-page-header">{message}</h1>
-//     )
-//   }
-// }
+class MembersSVG extends React.Component{
 
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
 
+    return (
+      <svg id="members-svg" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M529 896q-162 5-265 128h-134q-82 0-138-40.5t-56-118.5q0-353 124-353 6 0 43.5 21t97.5 42.5 119 21.5q67 0 133-23-5 37-5 66 0 139 81 256zm1071 637q0 120-73 189.5t-194 69.5h-874q-121 0-194-69.5t-73-189.5q0-53 3.5-103.5t14-109 26.5-108.5 43-97.5 62-81 85.5-53.5 111.5-20q10 0 43 21.5t73 48 107 48 135 21.5 135-21.5 107-48 73-48 43-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-1024-1277q0 106-75 181t-181 75-181-75-75-181 75-181 181-75 181 75 75 181zm704 384q0 159-112.5 271.5t-271.5 112.5-271.5-112.5-112.5-271.5 112.5-271.5 271.5-112.5 271.5 112.5 112.5 271.5zm576 225q0 78-56 118.5t-138 40.5h-134q-103-123-265-128 81-117 81-256 0-29-5-66 66 23 133 23 59 0 119-21.5t97.5-42.5 43.5-21q124 0 124 353zm-128-609q0 106-75 181t-181 75-181-75-75-181 75-181 181-75 181 75 75 181z"/></svg>
+   )
+  }
+}
 export default connect(
   (state) => {
     return {
