311506b6c16e7c1b2261976e5d8a7ab6673e65ef
shaya <shayasms128@gmail.com>
2017-09-24 07:44:39 +0300

    add something to script

--- app/playground/heroku_play.js
+++ app/playground/heroku_play.js
@@ -5,7 +5,20 @@ function sendXHR(url, callback) {
   var url = `http://liftii.herokuapp.com/`;
   axios.get(url)
   .then((res) => {
-    console.log('success');
+    console.log('success getting site');
+
+  }).catch((e) => {
+    console.log('error');
+    console.log(e);
+  });
+}
+
+function sendXHRCities() {
+  var url = 'https://todo22.herokuapp.com/cities';
+  var accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTk4NzcyMzIwYzZlZjAwMTFkZjhlZjAiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTAzMTY0MTk2fQ.nSsXaNBqzvR9rmMWa88E_bMjoSlPRYphwZ73dVad7Kc'
+  axios.get(url, {headers: {"x-auth":accessToken}})
+  .then((res) => {
+    console.log('success cities');
 
   }).catch((e) => {
     console.log('error');
@@ -15,7 +28,12 @@ function sendXHR(url, callback) {
 
 function infinite() {
     sendXHR('url/path');
+    sendXHRCities();
     setTimeout(infinite, 5 * 60 * 1000);
 }
 
 infinite();
+
+
+
+//
